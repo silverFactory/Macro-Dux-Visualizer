9d11d47d45ffa75f5db621495644223754572814
Will Studabaker <will.studabaker@gmail.com>
2021-04-16 14:40:05 -0400

    add bottom margin to navbar

--- macro-dux-visualizer/src/navbar.js
+++ macro-dux-visualizer/src/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
+import './navbar.css'
 
 export default function NavBar(){
 
