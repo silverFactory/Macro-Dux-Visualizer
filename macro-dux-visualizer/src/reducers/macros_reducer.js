9c6527a21d88cc3c14b8b2b7839b1f532d354d26
Will Studabaker <will.studabaker@gmail.com>
2021-04-20 19:40:54 -0400

    macro starting positions switched to 5

--- macro-dux-visualizer/src/reducers/macros_reducer.js
+++ macro-dux-visualizer/src/reducers/macros_reducer.js
@@ -1,14 +1,14 @@
 export default function macrosReducer(
   state = {
-    macro1: 0,
-    macro2: 0,
-    macro3: 0,
-    macro4: 0,
-    macro5: 0,
-    macro6: 0,
-    macro7: 0,
-    macro8: 0,
-    macro9: 0
+    macro1: 5,
+    macro2: 5,
+    macro3: 5,
+    macro4: 5,
+    macro5: 5,
+    macro6: 5,
+    macro7: 5,
+    macro8: 5,
+    macro9: 5
   },
   action){
   switch (action.type){
